d65e54cb7d9e69543633e8af8d82afc321cb1a6f
Lander Gray Carandang <143146712+GrayLander@users.noreply.github.com>
2023-11-16 23:25:42 +0800

    Add files via upload

--- db_nt3101.sql
+++ db_nt3101.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 16, 2023 at 04:19 PM
+-- Generation Time: Nov 16, 2023 at 04:25 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
@@ -34,6 +34,106 @@ CREATE TABLE `admin` (
   `password` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cabe candidate`
+--
+
+CREATE TABLE `cabe candidate` (
+  `SRCode` varchar(25) NOT NULL,
+  `FirstName` varchar(25) NOT NULL,
+  `LastName` varchar(25) NOT NULL,
+  `YearLevel` varchar(25) NOT NULL,
+  `Position` varchar(25) NOT NULL,
+  `Image` blob NOT NULL,
+  `numofVote` varchar(255) NOT NULL,
+  `Information` varchar(2555) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cabe candidate`
+--
+
+INSERT INTO `cabe candidate` (`SRCode`, `FirstName`, `LastName`, `YearLevel`, `Position`, `Image`, `numofVote`, `Information`) VALUES
+('asdfasdf', '123', 'sadf', '3', 'president', 0x322e706e67, '2', 'asdf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cas candidate`
+--
+
+CREATE TABLE `cas candidate` (
+  `SRCode` varchar(25) NOT NULL,
+  `FirstName` varchar(25) NOT NULL,
+  `LastName` varchar(25) NOT NULL,
+  `YearLevel` varchar(25) NOT NULL,
+  `Position` varchar(25) NOT NULL,
+  `Image` blob NOT NULL,
+  `numofVote` varchar(255) NOT NULL,
+  `Information` varchar(2555) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cete candidate`
+--
+
+CREATE TABLE `cete candidate` (
+  `SRCode` varchar(25) NOT NULL,
+  `FirstName` varchar(25) NOT NULL,
+  `LastName` varchar(25) NOT NULL,
+  `YearLevel` varchar(25) NOT NULL,
+  `Position` varchar(25) NOT NULL,
+  `Image` blob NOT NULL,
+  `numofVote` varchar(255) NOT NULL,
+  `Information` varchar(2555) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cics candidate`
+--
+
+CREATE TABLE `cics candidate` (
+  `SRCode` varchar(255) NOT NULL,
+  `FirstName` varchar(255) NOT NULL,
+  `LastName` varchar(255) NOT NULL,
+  `YearLevel` int(255) NOT NULL,
+  `Position` varchar(255) NOT NULL,
+  `Image` blob NOT NULL,
+  `numofVote` varchar(255) NOT NULL,
+  `Information` varchar(2555) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cics candidate`
+--
+
+INSERT INTO `cics candidate` (`SRCode`, `FirstName`, `LastName`, `YearLevel`, `Position`, `Image`, `numofVote`, `Information`) VALUES
+('asd', 'sdf', 'sadf', 0, 'president', '', '1', '1'),
+('sadf', '123', '123', 4, 'president', '', '2', 'asdf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cit candidate`
+--
+
+CREATE TABLE `cit candidate` (
+  `SRCode` varchar(25) NOT NULL,
+  `FirstName` varchar(25) NOT NULL,
+  `LastName` varchar(25) NOT NULL,
+  `YearLevel` varchar(25) NOT NULL,
+  `Position` varchar(25) NOT NULL,
+  `Image` blob NOT NULL,
+  `numofVote` varchar(255) NOT NULL,
+  `Information` varchar(2555) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Indexes for dumped tables
 --
@@ -43,6 +143,18 @@ CREATE TABLE `admin` (
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cabe candidate`
+--
+ALTER TABLE `cabe candidate`
+  ADD PRIMARY KEY (`SRCode`);
+
+--
+-- Indexes for table `cics candidate`
+--
+ALTER TABLE `cics candidate`
+  ADD PRIMARY KEY (`SRCode`);
 
 --
 -- AUTO_INCREMENT for dumped tables
