ebcb3759de070c775e7c279ed79b8f08aa61726e
John Matthew K. Tizon <143146816+JohnMatthewTizon@users.noreply.github.com>
2023-11-24 07:51:36 +0800

    Add files via upload

--- db_nt3101.sql
+++ db_nt3101.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 23, 2023 at 04:08 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 7.4.33
+-- Generation Time: Nov 12, 2023 at 07:12 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 7.4.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,89 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admindb`
+--
+
+CREATE TABLE `admindb` (
+  `adminID` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admindb`
+--
+
+INSERT INTO `admindb` (`adminID`, `password`) VALUES
+('admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contactus`
+--
+
+CREATE TABLE `contactus` (
+  `ID` int(11) NOT NULL,
+  `SR_Code` varchar(100) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `message` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contactus`
+--
+
+INSERT INTO `contactus` (`ID`, `SR_Code`, `subject`, `message`) VALUES
+(1, '21-30169', 'About Pin', 'Hi, Im paul and ang masasabi ko lang sa product niyo ay napakaganda at hindi maikukumpara sa iba pang product. Sana ituloy niyo lang ang magandang serbisyon niyo'),
+(2, '21-30169', 'About Pin', 'sjahkjhkjsahkjhsdfkjhasdkfjhasdkjfhaksjdhfaksjdhfakjsdhfakjsdhfaskdjhfaskjdhfaksdjfhaskdjfhaskdjfhasdkjfhasdkjfhasdkjfhasdkjfhasdkfjhaskdfjhaskdjfhaskdfjhaskdjfhaskjdfhaksjdhfaksjdhfkasjdfhaksdjfhaksdjhfaksjdhfaksjdhfaksjhfaskjdhfaksjdhfaskjdhfaskjdfhaskjdfhaskjdhfaskjdfhaskdjfhaskjfhasdkfjhaskdjfhasdkjfhaskdjfhasdkjfhasdkjfhaskjdhfaskjdhfaskjdhfaksjdhfkjashdfkjasdhfakjsdhfaskdjhfaksjdhfasdfasdfasdfasdfasdfasdfasdfasdfasdf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderdb`
+--
+
+CREATE TABLE `orderdb` (
+  `OrderID` int(100) NOT NULL,
+  `SR_Code` varchar(100) NOT NULL,
+  `Orderdate` date NOT NULL,
+  `Status` varchar(255) NOT NULL,
+  `OrderCost` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orderdb`
+--
+
+INSERT INTO `orderdb` (`OrderID`, `SR_Code`, `Orderdate`, `Status`, `OrderCost`) VALUES
+(97, '21-30169', '2023-11-07', 'Approved', 27),
+(114, '21-30169', '2023-11-10', 'Approved', 9);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderitems`
+--
+
+CREATE TABLE `orderitems` (
+  `OrderItemID` int(11) NOT NULL,
+  `OrderID` int(100) NOT NULL,
+  `ProductID` int(100) NOT NULL,
+  `Quantity` int(100) NOT NULL,
+  `TotalPrice` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orderitems`
+--
+
+INSERT INTO `orderitems` (`OrderItemID`, `OrderID`, `ProductID`, `Quantity`, `TotalPrice`) VALUES
+(117, 97, 1, 3, 27),
+(142, 114, 1, 1, 9);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `productdb`
 --
 
@@ -32,110 +115,89 @@ CREATE TABLE `productdb` (
   `ProductName` varchar(255) NOT NULL,
   `Price` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL,
-  `AvailStocks` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `AvailStocks` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `productdb`
 --
 
 INSERT INTO `productdb` (`ProductID`, `ProductName`, `Price`, `image`, `AvailStocks`) VALUES
-(64, 'Organizational Shrit', '550', 'product-1700743519.png', '200'),
-(65, 'Department shirt', '550', 'product-1700751825.png', '100');
+(1, 'Shot Glass', '9', '320140-removebg-preview.png', 7),
+(3, 'Pin', '4', 'IMS.png', 9),
+(4, 'Shirt', '200', 'EscuzarPaulAlvin_BSIT2203.png', 9),
+(5, 'Id Lace', '50', 'andre-benz-cXU6tNxhub0-unsplash.jpg', 9);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `productsuppliers`
+-- Table structure for table `shopcart`
 --
 
-CREATE TABLE `productsuppliers` (
-  `id` int(11) NOT NULL,
-  `supplier` int(11) NOT NULL,
-  `product` int(11) NOT NULL,
-  `updated_at` datetime NOT NULL,
-  `created_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `productsuppliers`
---
-
-INSERT INTO `productsuppliers` (`id`, `supplier`, `product`, `updated_at`, `created_at`) VALUES
-(16, 6, 64, '2023-11-23 13:45:19', '2023-11-23 13:45:19'),
-(17, 9, 65, '2023-11-23 16:03:45', '2023-11-23 16:03:45');
+CREATE TABLE `shopcart` (
+  `CartID` int(11) NOT NULL,
+  `SR_Code` varchar(100) NOT NULL,
+  `ProductID` int(100) NOT NULL,
+  `Quantity` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `product_supplier`
+-- Table structure for table `student_record`
 --
 
-CREATE TABLE `product_supplier` (
-  `id` int(11) NOT NULL,
-  `supplier` int(11) NOT NULL,
-  `product` int(11) NOT NULL,
-  `quantity_ordered` int(11) NOT NULL,
-  `quantity_received` int(11) NOT NULL,
-  `quantity_remaining` int(11) NOT NULL,
-  `status` varchar(20) NOT NULL,
-  `created_by` int(11) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `student_record` (
+  `SR_Code` varchar(100) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `dept` varchar(255) NOT NULL,
+  `prog_sec` varchar(255) NOT NULL,
+  `cnum` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student_record`
+--
+
+INSERT INTO `student_record` (`SR_Code`, `pass`, `firstname`, `lastname`, `email`, `dept`, `prog_sec`, `cnum`) VALUES
+('21-30169', 'Pol', 'Paul Alvin', 'Tolentino', '21-30169@g.batstate-u.edu.ph', 'CICS', 'BSIT-NT-3101', '09090774577'),
+('21-33112', 'Lord', 'Wingell Lord', 'Vinas', 'ichigokurosaki@gmail.com', 'CICS', 'BSIT-NT-3101', '09445278541');
 
 -- --------------------------------------------------------
-
---
--- Table structure for table `suppliers`
---
-
-CREATE TABLE `suppliers` (
-  `id` int(11) NOT NULL,
-  `supplier_name` varchar(191) NOT NULL,
-  `supplier_location` varchar(191) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `created_by` int(11) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `suppliers`
---
-
-INSERT INTO `suppliers` (`id`, `supplier_name`, `supplier_location`, `email`, `created_by`, `created_at`, `updated_at`) VALUES
-(6, 'test_supplier', 'lipa', 'testsupplier@gmail.com', 1, '2023-11-23 13:38:05', '2023-11-23 13:38:05'),
-(9, 'test_supplier2', 'lipa2', 'testsupplier2@gmail.com', 1, '2023-11-23 16:03:28', '2023-11-23 16:03:28');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `first_name` varchar(100) NOT NULL,
-  `last_name` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `created_at`, `updated_at`) VALUES
-(1, 'johnMatthew', 'Tizon', 'johnmatthew@gmail.com', 'johnmatthew', '2023-11-15 11:00:22', '2023-11-15 11:00:22'),
-(34, 'fhel', 'Tandingan', 'johnmatthew@gmail.com', 'johnmatthew', '2023-11-22 09:12:52', '2023-11-22 09:12:52'),
-(36, 'kian', 'kalaw', 'kian@gmail.com', 'johnmatthew', '2023-11-23 15:58:47', '2023-11-23 15:58:47');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admindb`
+--
+ALTER TABLE `admindb`
+  ADD PRIMARY KEY (`adminID`);
+
+--
+-- Indexes for table `contactus`
+--
+ALTER TABLE `contactus`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `orderdb`
+--
+ALTER TABLE `orderdb`
+  ADD PRIMARY KEY (`OrderID`),
+  ADD KEY `SR-Code` (`SR_Code`);
+
+--
+-- Indexes for table `orderitems`
+--
+ALTER TABLE `orderitems`
+  ADD PRIMARY KEY (`OrderItemID`),
+  ADD KEY `OrderID` (`OrderID`),
+  ADD KEY `ProductID` (`ProductID`);
 
 --
 -- Indexes for table `productdb`
@@ -144,80 +206,68 @@ ALTER TABLE `productdb`
   ADD PRIMARY KEY (`ProductID`);
 
 --
--- Indexes for table `productsuppliers`
+-- Indexes for table `shopcart`
 --
-ALTER TABLE `productsuppliers`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `product` (`product`),
-  ADD KEY `supplier` (`supplier`);
+ALTER TABLE `shopcart`
+  ADD PRIMARY KEY (`CartID`);
 
 --
--- Indexes for table `product_supplier`
+-- Indexes for table `student_record`
 --
-ALTER TABLE `product_supplier`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `supplier` (`supplier`),
-  ADD KEY `product` (`product`),
-  ADD KEY `created_by` (`created_by`);
+ALTER TABLE `student_record`
+  ADD PRIMARY KEY (`SR_Code`);
 
 --
--- Indexes for table `suppliers`
---
-ALTER TABLE `suppliers`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `created_by` (`created_by`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `contactus`
+--
+ALTER TABLE `contactus`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `orderdb`
+--
+ALTER TABLE `orderdb`
+  MODIFY `OrderID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=115;
+
+--
+-- AUTO_INCREMENT for table `orderitems`
+--
+ALTER TABLE `orderitems`
+  MODIFY `OrderItemID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=143;
+
+--
 -- AUTO_INCREMENT for table `productdb`
 --
 ALTER TABLE `productdb`
-  MODIFY `ProductID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
+  MODIFY `ProductID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `productsuppliers`
+-- AUTO_INCREMENT for table `shopcart`
 --
-ALTER TABLE `productsuppliers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
-
---
--- AUTO_INCREMENT for table `product_supplier`
---
-ALTER TABLE `product_supplier`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `suppliers`
---
-ALTER TABLE `suppliers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+ALTER TABLE `shopcart`
+  MODIFY `CartID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `product_supplier`
+-- Constraints for table `orderdb`
 --
-ALTER TABLE `product_supplier`
-  ADD CONSTRAINT `product_supplier_ibfk_1` FOREIGN KEY (`supplier`) REFERENCES `suppliers` (`id`),
-  ADD CONSTRAINT `product_supplier_ibfk_2` FOREIGN KEY (`product`) REFERENCES `productdb` (`ProductID`),
-  ADD CONSTRAINT `product_supplier_ibfk_3` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`);
+ALTER TABLE `orderdb`
+  ADD CONSTRAINT `SR-Code` FOREIGN KEY (`SR_Code`) REFERENCES `student_record` (`SR_Code`);
+
+--
+-- Constraints for table `orderitems`
+--
+ALTER TABLE `orderitems`
+  ADD CONSTRAINT `OrderID` FOREIGN KEY (`OrderID`) REFERENCES `orderdb` (`OrderID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ProductID` FOREIGN KEY (`ProductID`) REFERENCES `productdb` (`ProductID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
