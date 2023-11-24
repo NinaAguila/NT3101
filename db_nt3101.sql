bc22a7fcc0cd28c51c1495586f04afc181dffc10
Rochelle Ann D. Miranda <143161206+RochelleMiranda3@users.noreply.github.com>
2023-11-24 11:39:02 +0800

    Add files via upload

--- db_nt3101.sql
+++ db_nt3101.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3308
--- Generation Time: Oct 26, 2023 at 11:34 PM
--- Server version: 8.0.18
--- PHP Version: 7.3.12
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 24, 2023 at 03:09 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,8 +18,105 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_nt3101`
+-- Database: `neww`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booktable`
+--
+
+DROP TABLE IF EXISTS `booktable`;
+CREATE TABLE IF NOT EXISTS `booktable` (
+  `BookID` int NOT NULL AUTO_INCREMENT,
+  `Author` varchar(255) NOT NULL,
+  `BookNames` varchar(255) NOT NULL,
+  `Description` varchar(255) NOT NULL,
+  `Category` varchar(255) NOT NULL,
+  `ISBN` varchar(255) NOT NULL,
+  PRIMARY KEY (`BookID`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logintable`
+--
+
+DROP TABLE IF EXISTS `logintable`;
+CREATE TABLE IF NOT EXISTS `logintable` (
+  `UserID` int NOT NULL AUTO_INCREMENT,
+  `studid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `empid` int NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  PRIMARY KEY (`UserID`)
+) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservehistorytable`
+--
+
+DROP TABLE IF EXISTS `reservehistorytable`;
+CREATE TABLE IF NOT EXISTS `reservehistorytable` (
+  `HistoryID` int NOT NULL,
+  `ReserveID` int NOT NULL,
+  `StudentID` int NOT NULL,
+  `BookID` int NOT NULL,
+  `DateReserve` varchar(255) NOT NULL,
+  `DateReturn` varchar(255) NOT NULL,
+  `Status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`HistoryID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservetable`
+--
+
+DROP TABLE IF EXISTS `reservetable`;
+CREATE TABLE IF NOT EXISTS `reservetable` (
+  `ReserveID` int NOT NULL AUTO_INCREMENT,
+  `studid` int NOT NULL,
+  `BookID` int NOT NULL,
+  `Status` varchar(255) NOT NULL,
+  PRIMARY KEY (`ReserveID`)
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `returntable`
+--
+
+DROP TABLE IF EXISTS `returntable`;
+CREATE TABLE IF NOT EXISTS `returntable` (
+  `ReturnID` int NOT NULL AUTO_INCREMENT,
+  `ReserveID` int DEFAULT NULL,
+  `DateReserve` date DEFAULT NULL,
+  `DateReturn` date DEFAULT NULL,
+  `Status` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`ReturnID`),
+  KEY `ReserveID` (`ReserveID`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student table`
+--
+
+DROP TABLE IF EXISTS `student table`;
+CREATE TABLE IF NOT EXISTS `student table` (
+  `StudentID` int NOT NULL AUTO_INCREMENT,
+  `studid` int NOT NULL,
+  `SR-Code` varchar(255) NOT NULL,
+  `Year & Section` varchar(255) NOT NULL,
+  PRIMARY KEY (`StudentID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -30,7 +126,7 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `tbempinfo`;
 CREATE TABLE IF NOT EXISTS `tbempinfo` (
-  `empid` int(11) NOT NULL AUTO_INCREMENT,
+  `empid` int NOT NULL AUTO_INCREMENT,
   `lastname` varchar(25) NOT NULL,
   `firstname` varchar(25) NOT NULL,
   `department` varchar(20) NOT NULL,
@@ -52,7 +148,7 @@ INSERT INTO `tbempinfo` (`empid`, `lastname`, `firstname`, `department`) VALUES
 
 DROP TABLE IF EXISTS `tbstudinfo`;
 CREATE TABLE IF NOT EXISTS `tbstudinfo` (
-  `studid` int(11) NOT NULL AUTO_INCREMENT,
+  `studid` int NOT NULL AUTO_INCREMENT,
   `lastname` varchar(25) NOT NULL,
   `firstname` varchar(25) NOT NULL,
   `course` varchar(20) NOT NULL,
