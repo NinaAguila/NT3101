55c3e3674ca29c54c303385f0158070a6940e9e6
Rochelle Ann D. Miranda <143161206+RochelleMiranda3@users.noreply.github.com>
2023-11-26 20:40:05 +0800

    Add files via upload

--- db_nt3101.sql
+++ db_nt3101.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 24, 2023 at 03:49 AM
+-- Generation Time: Nov 26, 2023 at 10:44 AM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -36,69 +36,22 @@ CREATE TABLE IF NOT EXISTS `booktable` (
   `Category` varchar(255) NOT NULL,
   `ISBN` varchar(255) NOT NULL,
   PRIMARY KEY (`BookID`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `booktable`
---
-
-INSERT INTO `booktable` (`BookID`, `Author`, `BookNames`, `Description`, `Category`, `ISBN`) VALUES
-(10, 'William Adams', 'Fantasy Realm', 'Immerse yourself in a world of fantasy', 'Fantasy', '5544332211'),
-(9, 'Olivia Davis', 'Business Strategies', 'Navigating the corporate world', 'Business', '1122334455'),
-(7, 'Sophia Taylor', 'Travel Chronicles', 'Journeying through diverse landscapes', 'Travel', '9012345678'),
-(8, 'Daniel Miller', 'Poetry Collection', 'Verses that touch the soul', 'Poetry', '4567890123'),
-(6, 'Michael Lee', 'Science Wonders', 'Unraveling the secrets of the universe', 'Science', '2345678901'),
-(5, 'Emily White', 'Artistic Expressions', 'Discovering the world of art', 'Art', '7890123456'),
-(4, 'Bob Brown', 'History Uncovered', 'Exploring the mysteries of the past', 'History', '3456789012'),
-(3, 'Alice Johnson', 'Cooking Delights', 'Recipes for delicious meals', 'Cooking', '5678901234'),
-(1, 'John Doe', 'The Great Novel', 'An exciting novel about adventure', 'Fiction', '1234567890'),
-(2, 'Jane Smith', 'Programming Basics', 'A guide to programming for beginners', 'Non-Fiction', '9876543210');
+) ENGINE=MyISAM AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `emplogin`
+-- Table structure for table `employe table`
 --
 
-DROP TABLE IF EXISTS `emplogin`;
-CREATE TABLE IF NOT EXISTS `emplogin` (
-  `EmployeeID` int NOT NULL AUTO_INCREMENT,
-  `empid` int NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`EmployeeID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `logintable`
---
-
-DROP TABLE IF EXISTS `logintable`;
-CREATE TABLE IF NOT EXISTS `logintable` (
+DROP TABLE IF EXISTS `employe table`;
+CREATE TABLE IF NOT EXISTS `employe table` (
   `UserID` int NOT NULL AUTO_INCREMENT,
-  `studid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `Password` varchar(255) NOT NULL,
+  `empid` int NOT NULL,
+  `contact` int NOT NULL,
+  `password` varchar(255) NOT NULL,
   PRIMARY KEY (`UserID`)
-) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `reservehistorytable`
---
-
-DROP TABLE IF EXISTS `reservehistorytable`;
-CREATE TABLE IF NOT EXISTS `reservehistorytable` (
-  `HistoryID` int NOT NULL,
-  `ReserveID` int NOT NULL,
-  `StudentID` int NOT NULL,
-  `BookID` int NOT NULL,
-  `DateReserve` varchar(255) NOT NULL,
-  `DateReturn` varchar(255) NOT NULL,
-  `Status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  PRIMARY KEY (`HistoryID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -109,11 +62,12 @@ CREATE TABLE IF NOT EXISTS `reservehistorytable` (
 DROP TABLE IF EXISTS `reservetable`;
 CREATE TABLE IF NOT EXISTS `reservetable` (
   `ReserveID` int NOT NULL AUTO_INCREMENT,
-  `studid` int NOT NULL,
+  `userID` int DEFAULT NULL,
   `BookID` int NOT NULL,
+  `DateReserve` date DEFAULT NULL,
   `Status` varchar(255) NOT NULL,
   PRIMARY KEY (`ReserveID`)
-) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=181 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -124,13 +78,13 @@ CREATE TABLE IF NOT EXISTS `reservetable` (
 DROP TABLE IF EXISTS `returntable`;
 CREATE TABLE IF NOT EXISTS `returntable` (
   `ReturnID` int NOT NULL AUTO_INCREMENT,
-  `ReserveID` int DEFAULT NULL,
-  `DateReserve` date DEFAULT NULL,
+  `ReserveID` int NOT NULL,
+  `empid` int DEFAULT NULL,
+  `DateApproved` date DEFAULT NULL,
   `DateReturn` date DEFAULT NULL,
   `Status` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`ReturnID`),
-  KEY `ReserveID` (`ReserveID`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`ReturnID`)
+) ENGINE=MyISAM AUTO_INCREMENT=53 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -140,12 +94,13 @@ CREATE TABLE IF NOT EXISTS `returntable` (
 
 DROP TABLE IF EXISTS `student table`;
 CREATE TABLE IF NOT EXISTS `student table` (
-  `StudentID` int NOT NULL AUTO_INCREMENT,
+  `userID` int NOT NULL AUTO_INCREMENT,
   `studid` int NOT NULL,
   `SR-Code` varchar(255) NOT NULL,
-  `Year & Section` varchar(255) NOT NULL,
-  PRIMARY KEY (`StudentID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `Section` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  PRIMARY KEY (`userID`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -182,7 +137,7 @@ CREATE TABLE IF NOT EXISTS `tbstudinfo` (
   `firstname` varchar(25) NOT NULL,
   `course` varchar(20) NOT NULL,
   PRIMARY KEY (`studid`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tbstudinfo`
