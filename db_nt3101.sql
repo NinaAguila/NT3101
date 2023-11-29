ae444c6788df8f7c9abb2d13bbf7c02f9f5770b8
Lander Gray Carandang <143146712+GrayLander@users.noreply.github.com>
2023-11-29 19:11:11 +0800

    Add files via upload

--- db_nt3101.sql
+++ db_nt3101.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 27, 2023 at 05:51 AM
+-- Generation Time: Nov 29, 2023 at 12:08 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
@@ -20,8 +20,134 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_nt3101`
 --
+CREATE TABLE `tbempinfo` (
+  `empid` int(11) NOT NULL,
+  `lastname` varchar(25) NOT NULL,
+  `firstname` varchar(25) NOT NULL,
+  `department` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbempinfo`
+--
+
+INSERT INTO `tbempinfo` (`empid`, `lastname`, `firstname`, `department`) VALUES
+(1, 'aguila', 'nina', 'cics');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbempinfo`
+--
+ALTER TABLE `tbempinfo`
+  ADD PRIMARY KEY (`empid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbempinfo`
+--
+ALTER TABLE `tbempinfo`
+  MODIFY `empid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbstudinfo`
+--
+
+CREATE TABLE `tbstudinfo` (
+  `studid` int(11) NOT NULL,
+  `lastname` varchar(25) NOT NULL,
+  `firstname` varchar(25) NOT NULL,
+  `course` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbstudinfo`
+--
+
+INSERT INTO `tbstudinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
+(1, 'parker', 'peter', 'BSIT'),
+(2, 'kent', 'clark', 'bscs');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbstudinfo`
+--
+ALTER TABLE `tbstudinfo`
+  ADD PRIMARY KEY (`studid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbstudinfo`
+--
+ALTER TABLE `tbstudinfo`
+  MODIFY `studid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
+
+
+CREATE TABLE `candidates` (
+  `studid` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `course` varchar(255) NOT NULL,
+  `yearlevel` varchar(255) NOT NULL,
+  `position` varchar(255) NOT NULL,
+  `image` blob NOT NULL,
+  `numofVote` varchar(255) NOT NULL,
+  `information` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `candidates`
+--
+
+INSERT INTO `candidates` (`studid`, `lastname`, `firstname`, `course`, `yearlevel`, `position`, `image`, `numofVote`, `information`) VALUES
+('1', 'parkers', 'peters', 'CETE', '4', 'Peace Officer', 0x75706c6f6164732f70726f66696c65312e6a7067, '2', 'asdf'),
+('2', 'kent', 'clark', 'CETE', '', 'Peace Officer', 0x70726f66696c65322e6a666966, '2', 'sadf');
+COMMIT;
+
+
+CREATE TABLE `votersinfo` (
+  `studid` varchar(25) NOT NULL,
+  `yearlevel` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `hasVotedPresident` int(11) DEFAULT 0,
+  `hasVotedVicePresident` int(11) DEFAULT 0,
+  `hasVotedSecretary` int(11) DEFAULT 0,
+  `hasVotedPeaceOfficer` int(11) DEFAULT 0
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `votersinfo`
+--
+
+INSERT INTO `votersinfo` (`studid`, `yearlevel`, `Password`, `hasVotedPresident`, `hasVotedVicePresident`, `hasVotedSecretary`, `hasVotedPeaceOfficer`) VALUES
+('1', '4', 'user', 0, 0, 0, 0),
+('2', '3', 'user', 0, 0, 0, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `votersinfo`
+--
+ALTER TABLE `votersinfo`
+  ADD PRIMARY KEY (`studid`);
+COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
