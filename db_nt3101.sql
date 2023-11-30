02079175a06238cf011413c4b7c874791fb016e4
John Matthew K. Tizon <143146816+JohnMatthewTizon@users.noreply.github.com>
2023-11-30 12:08:39 +0800

    Add files via upload

--- db_nt3101.sql
+++ db_nt3101.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 24, 2023 at 04:47 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 7.4.33
+-- Generation Time: Nov 30, 2023 at 12:28 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,48 +24,71 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `productdb`
+-- Table structure for table `order_product_history`
 --
 
-CREATE TABLE `productdb` (
+CREATE TABLE `order_product_history` (
+  `id` int(11) NOT NULL,
+  `order_product_id` int(11) NOT NULL,
+  `qty_received` int(11) NOT NULL,
+  `date_received` datetime NOT NULL,
+  `date_updated` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
   `id` int(100) NOT NULL,
   `ProductName` varchar(255) NOT NULL,
   `adminId` int(11) NOT NULL,
   `Price` varchar(255) NOT NULL,
-  `image` varchar(255) NOT NULL
+  `image` varchar(255) NOT NULL,
+  `stock` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `productdb`
+-- Dumping data for table `products`
 --
 
-INSERT INTO `productdb` (`id`, `ProductName`, `adminId`, `Price`, `image`) VALUES
-(64, 'Organizational Shrit', 1, '550', 'product-1700743519.png');
+INSERT INTO `products` (`id`, `ProductName`, `adminId`, `Price`, `image`, `stock`) VALUES
+(73, 'Department shirt', 1, '400', 'product-1700985708.png', 30);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `product_out`
+-- Table structure for table `productsuppliers`
 --
 
-CREATE TABLE `product_out` (
+CREATE TABLE `productsuppliers` (
   `id` int(11) NOT NULL,
   `supplier` int(11) NOT NULL,
   `product` int(11) NOT NULL,
-  `quantity_ordered` int(11) NOT NULL
+  `updated_at` datetime NOT NULL,
+  `created_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `product_supplier`
+-- Table structure for table `product_in`
 --
 
-CREATE TABLE `product_supplier` (
+CREATE TABLE `product_in` (
   `id` int(11) NOT NULL,
   `supplier` int(11) NOT NULL,
   `product` int(11) NOT NULL,
-  `quantity_received` int(11) NOT NULL
+  `quantity_ordered` int(11) NOT NULL,
+  `quantity_received` int(11) NOT NULL,
+  `quantity_remaining` int(11) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `batch` varchar(200) NOT NULL,
+  `adminId` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -78,16 +101,16 @@ CREATE TABLE `suppliers` (
   `id` int(11) NOT NULL,
   `supplier_name` varchar(191) NOT NULL,
   `supplier_location` varchar(191) NOT NULL,
-  `email` varchar(100) NOT NULL
+  `email` varchar(100) NOT NULL,
+  `adminId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `suppliers`
 --
 
-INSERT INTO `suppliers` (`id`, `supplier_name`, `supplier_location`, `email`) VALUES
-(6, 'test_supplier', 'lipa', 'testsupplier@gmail.com'),
-(9, 'test_supplier2', 'lipa2', 'testsupplier2@gmail.com');
+INSERT INTO `suppliers` (`id`, `supplier_name`, `supplier_location`, `email`, `adminId`) VALUES
+(6, 'Department Shirt Supplier', 'balintawak', 'DepartmentShirtSupplier@gmail.com', 1);
 
 -- --------------------------------------------------------
 
@@ -107,29 +130,7 @@ CREATE TABLE `tbempinfo` (
 --
 
 INSERT INTO `tbempinfo` (`empid`, `lastname`, `firstname`, `department`) VALUES
-(1, '', '', 'AICSS'),
-(2, '', '', 'cics');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbstudinfo`
---
-
-CREATE TABLE `tbstudinfo` (
-  `studid` int(11) NOT NULL,
-  `lastname` varchar(25) NOT NULL,
-  `firstname` varchar(25) NOT NULL,
-  `course` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `tbstudinfo`
---
-
-INSERT INTO `tbstudinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
-(1, 'parker', 'peter', 'bsit'),
-(2, 'kent', 'clark', 'bscs');
+(1, 'aguila', 'nina', 'cics');
 
 -- --------------------------------------------------------
 
@@ -151,44 +152,55 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `password`, `created_at`, `updated_at`, `emp`) VALUES
-(1, 'johnmatthew@gmail.com', 'johnmatthew', '2023-11-15 11:00:22', '2023-11-15 11:00:22', 1),
-(37, 'kyle@gmail.com', 'kyle123', '2023-11-24 00:25:33', '2023-11-24 00:25:33', 2);
+(1, 'johnmatthew300@gmail.com', 'johnmatthew', '2023-11-15 11:00:22', '2023-11-26 10:05:23', 1);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `productdb`
+-- Indexes for table `order_product_history`
 --
-ALTER TABLE `productdb`
+ALTER TABLE `order_product_history`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `created_by` (`adminId`);
+  ADD KEY `order_product_id` (`order_product_id`);
 
 --
--- Indexes for table `product_supplier`
+-- Indexes for table `products`
 --
-ALTER TABLE `product_supplier`
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `adminId` (`adminId`);
+
+--
+-- Indexes for table `productsuppliers`
+--
+ALTER TABLE `productsuppliers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product` (`product`),
+  ADD KEY `supplier` (`supplier`);
+
+--
+-- Indexes for table `product_in`
+--
+ALTER TABLE `product_in`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `product_supplier_ibfk_1` (`supplier`),
-  ADD KEY `product_supplier_ibfk_2` (`product`);
+  ADD KEY `product_supplier_ibfk_2` (`product`),
+  ADD KEY `adminId` (`adminId`);
 
 --
 -- Indexes for table `suppliers`
 --
 ALTER TABLE `suppliers`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `adminId` (`adminId`);
 
 --
 -- Indexes for table `tbempinfo`
 --
 ALTER TABLE `tbempinfo`
   ADD PRIMARY KEY (`empid`);
-
---
--- Indexes for table `tbstudinfo`
---
-ALTER TABLE `tbstudinfo`
-  ADD PRIMARY KEY (`studid`);
 
 --
 -- Indexes for table `users`
@@ -202,51 +214,83 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `productdb`
+-- AUTO_INCREMENT for table `order_product_history`
 --
-ALTER TABLE `productdb`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+ALTER TABLE `order_product_history`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+
+--
+-- AUTO_INCREMENT for table `productsuppliers`
+--
+ALTER TABLE `productsuppliers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `product_in`
+--
+ALTER TABLE `product_in`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `suppliers`
 --
 ALTER TABLE `suppliers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `tbempinfo`
 --
 ALTER TABLE `tbempinfo`
-  MODIFY `empid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `tbstudinfo`
---
-ALTER TABLE `tbstudinfo`
-  MODIFY `studid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `empid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `productdb`
+-- Constraints for table `order_product_history`
 --
-ALTER TABLE `productdb`
-  ADD CONSTRAINT `productdb_ibfk_1` FOREIGN KEY (`adminId`) REFERENCES `tbempinfo` (`empid`);
+ALTER TABLE `order_product_history`
+  ADD CONSTRAINT `order_product_history_ibfk_1` FOREIGN KEY (`order_product_id`) REFERENCES `product_in` (`id`);
 
 --
--- Constraints for table `product_supplier`
+-- Constraints for table `products`
 --
-ALTER TABLE `product_supplier`
-  ADD CONSTRAINT `product_supplier_ibfk_1` FOREIGN KEY (`supplier`) REFERENCES `suppliers` (`id`),
-  ADD CONSTRAINT `product_supplier_ibfk_2` FOREIGN KEY (`product`) REFERENCES `productdb` (`id`);
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`adminId`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `productsuppliers`
+--
+ALTER TABLE `productsuppliers`
+  ADD CONSTRAINT `productsuppliers_ibfk_1` FOREIGN KEY (`product`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `productsuppliers_ibfk_2` FOREIGN KEY (`supplier`) REFERENCES `suppliers` (`id`);
+
+--
+-- Constraints for table `product_in`
+--
+ALTER TABLE `product_in`
+  ADD CONSTRAINT `product_in_ibfk_1` FOREIGN KEY (`supplier`) REFERENCES `suppliers` (`id`),
+  ADD CONSTRAINT `product_in_ibfk_2` FOREIGN KEY (`product`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `product_in_ibfk_3` FOREIGN KEY (`adminId`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  ADD CONSTRAINT `suppliers_ibfk_1` FOREIGN KEY (`adminId`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `users`
