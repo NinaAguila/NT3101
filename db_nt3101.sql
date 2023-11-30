82169decffd89164b7ba235a1fdc95017db37751
Lander Gray Carandang <143146712+GrayLander@users.noreply.github.com>
2023-11-30 13:46:43 +0800

    Add files via upload

--- db_nt3101.sql
+++ db_nt3101.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 30, 2023 at 05:32 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 7.4.33
+-- Generation Time: Nov 30, 2023 at 12:28 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,44 +24,95 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbstudinfo`
+-- Table structure for table `order_product_history`
 --
 
-CREATE TABLE `tbstudinfo` (
-  `studid` int(11) NOT NULL,
-  `lastname` varchar(25) NOT NULL,
-  `firstname` varchar(25) NOT NULL,
-  `course` varchar(20) NOT NULL
+CREATE TABLE `order_product_history` (
+  `id` int(11) NOT NULL,
+  `order_product_id` int(11) NOT NULL,
+  `qty_received` int(11) NOT NULL,
+  `date_received` datetime NOT NULL,
+  `date_updated` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(100) NOT NULL,
+  `ProductName` varchar(255) NOT NULL,
+  `adminId` int(11) NOT NULL,
+  `Price` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `stock` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `tbstudinfo`
+-- Dumping data for table `products`
 --
 
-INSERT INTO `tbstudinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
-(1, 'parker', 'peter', 'BSIT'),
-(2, 'kent', 'clark', 'bscs');
+INSERT INTO `products` (`id`, `ProductName`, `adminId`, `Price`, `image`, `stock`) VALUES
+(73, 'Department shirt', 1, '400', 'product-1700985708.png', 30);
+
+-- --------------------------------------------------------
 
 --
--- Indexes for dumped tables
+-- Table structure for table `productsuppliers`
 --
 
---
--- Indexes for table `tbstudinfo`
---
-ALTER TABLE `tbstudinfo`
-  ADD PRIMARY KEY (`studid`);
+CREATE TABLE `productsuppliers` (
+  `id` int(11) NOT NULL,
+  `supplier` int(11) NOT NULL,
+  `product` int(11) NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Table structure for table `product_in`
 --
 
+CREATE TABLE `product_in` (
+  `id` int(11) NOT NULL,
+  `supplier` int(11) NOT NULL,
+  `product` int(11) NOT NULL,
+  `quantity_ordered` int(11) NOT NULL,
+  `quantity_received` int(11) NOT NULL,
+  `quantity_remaining` int(11) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `batch` varchar(200) NOT NULL,
+  `adminId` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
 --
--- AUTO_INCREMENT for table `tbstudinfo`
+-- Table structure for table `suppliers`
 --
-ALTER TABLE `tbstudinfo`
-  MODIFY `studid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
+
+CREATE TABLE `suppliers` (
+  `id` int(11) NOT NULL,
+  `supplier_name` varchar(191) NOT NULL,
+  `supplier_location` varchar(191) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `adminId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `suppliers`
+--
+
+INSERT INTO `suppliers` (`id`, `supplier_name`, `supplier_location`, `email`, `adminId`) VALUES
+(6, 'Department Shirt Supplier', 'balintawak', 'DepartmentShirtSupplier@gmail.com', 1);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `tbempinfo`
@@ -81,9 +132,69 @@ CREATE TABLE `tbempinfo` (
 INSERT INTO `tbempinfo` (`empid`, `lastname`, `firstname`, `department`) VALUES
 (1, 'aguila', 'nina', 'cics');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `emp` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `created_at`, `updated_at`, `emp`) VALUES
+(1, 'johnmatthew300@gmail.com', 'johnmatthew', '2023-11-15 11:00:22', '2023-11-26 10:05:23', 1);
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `order_product_history`
+--
+ALTER TABLE `order_product_history`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order_product_id` (`order_product_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `adminId` (`adminId`);
+
+--
+-- Indexes for table `productsuppliers`
+--
+ALTER TABLE `productsuppliers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product` (`product`),
+  ADD KEY `supplier` (`supplier`);
+
+--
+-- Indexes for table `product_in`
+--
+ALTER TABLE `product_in`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_supplier_ibfk_1` (`supplier`),
+  ADD KEY `product_supplier_ibfk_2` (`product`),
+  ADD KEY `adminId` (`adminId`);
+
+--
+-- Indexes for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `adminId` (`adminId`);
 
 --
 -- Indexes for table `tbempinfo`
@@ -92,72 +203,102 @@ ALTER TABLE `tbempinfo`
   ADD PRIMARY KEY (`empid`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `emp` (`emp`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `order_product_history`
+--
+ALTER TABLE `order_product_history`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+
+--
+-- AUTO_INCREMENT for table `productsuppliers`
+--
+ALTER TABLE `productsuppliers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `product_in`
+--
+ALTER TABLE `product_in`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `tbempinfo`
 --
 ALTER TABLE `tbempinfo`
-  MODIFY `empid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `empid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `order_product_history`
+--
+ALTER TABLE `order_product_history`
+  ADD CONSTRAINT `order_product_history_ibfk_1` FOREIGN KEY (`order_product_id`) REFERENCES `product_in` (`id`);
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`adminId`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `productsuppliers`
+--
+ALTER TABLE `productsuppliers`
+  ADD CONSTRAINT `productsuppliers_ibfk_1` FOREIGN KEY (`product`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `productsuppliers_ibfk_2` FOREIGN KEY (`supplier`) REFERENCES `suppliers` (`id`);
+
+--
+-- Constraints for table `product_in`
+--
+ALTER TABLE `product_in`
+  ADD CONSTRAINT `product_in_ibfk_1` FOREIGN KEY (`supplier`) REFERENCES `suppliers` (`id`),
+  ADD CONSTRAINT `product_in_ibfk_2` FOREIGN KEY (`product`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `product_in_ibfk_3` FOREIGN KEY (`adminId`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  ADD CONSTRAINT `suppliers_ibfk_1` FOREIGN KEY (`adminId`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`emp`) REFERENCES `tbempinfo` (`empid`);
 COMMIT;
 
-CREATE TABLE `votersinfo` (
-  `id` int(100) NOT NULL,
-  `studid` int(11) NOT NULL,
-  `yearlevel` varchar(255) NOT NULL,
-  `Password` varchar(255) NOT NULL,
-  `hasVotedPresident` tinyint(1) DEFAULT 0,
-  `hasVotedVicePresident` tinyint(1) DEFAULT 0,
-  `hasVotedSecretary` tinyint(1) DEFAULT 0,
-  `hasVotedPeaceOfficer` tinyint(1) DEFAULT 0
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `votersinfo`
---
-
-INSERT INTO `votersinfo` (`id`, `studid`, `yearlevel`, `Password`, `hasVotedPresident`, `hasVotedVicePresident`, `hasVotedSecretary`, `hasVotedPeaceOfficer`) VALUES
-(1, 1, '4', 'user', 1, 0, 0, 0),
-(2, 2, '3', 'user', 0, 0, 0, 0);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `votersinfo`
---
-ALTER TABLE `votersinfo`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `votersinfo`
---
-ALTER TABLE `votersinfo`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
-
-
-CREATE TABLE `candidates` (
-  `studid` varchar(255) NOT NULL,
-  `lastname` varchar(255) NOT NULL,
-  `firstname` varchar(255) NOT NULL,
-  `course` varchar(255) NOT NULL,
-  `yearlevel` varchar(255) NOT NULL,
-  `position` varchar(255) NOT NULL,
-  `image` blob NOT NULL,
-  `numofVote` varchar(255) NOT NULL,
-  `information` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-COMMIT;
-
-ALTER TABLE `votersinfo`
-ADD FOREIGN KEY (`studid`) REFERENCES `tbstudinfo`(`studid`);
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
