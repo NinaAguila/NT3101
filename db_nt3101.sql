e9d2afa1a7b71884671d09aa13e9e332ebc1cf6b
Rochelle Ann D. Miranda <143161206+RochelleMiranda3@users.noreply.github.com>
2023-12-01 08:27:45 +0800

    Add files via upload

--- db_nt3101.sql
+++ db_nt3101.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 26, 2023 at 10:44 AM
+-- Generation Time: Nov 24, 2023 at 04:24 AM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -18,89 +18,31 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_nt3101`
+-- Database: `db_nt3101s`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `booktable`
+-- Table structure for table `tbadminacc`
 --
 
-DROP TABLE IF EXISTS `booktable`;
-CREATE TABLE IF NOT EXISTS `booktable` (
-  `BookID` int NOT NULL AUTO_INCREMENT,
-  `Author` varchar(255) NOT NULL,
-  `BookNames` varchar(255) NOT NULL,
-  `Description` varchar(255) NOT NULL,
-  `Category` varchar(255) NOT NULL,
-  `ISBN` varchar(255) NOT NULL,
-  PRIMARY KEY (`BookID`)
-) ENGINE=MyISAM AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `employe table`
---
-
-DROP TABLE IF EXISTS `employe table`;
-CREATE TABLE IF NOT EXISTS `employe table` (
-  `UserID` int NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `tbadminacc`;
+CREATE TABLE IF NOT EXISTS `tbadminacc` (
+  `adminid` int NOT NULL AUTO_INCREMENT,
   `empid` int NOT NULL,
-  `contact` int NOT NULL,
+  `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`UserID`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `reservetable`
---
-
-DROP TABLE IF EXISTS `reservetable`;
-CREATE TABLE IF NOT EXISTS `reservetable` (
-  `ReserveID` int NOT NULL AUTO_INCREMENT,
-  `userID` int DEFAULT NULL,
-  `BookID` int NOT NULL,
-  `DateReserve` date DEFAULT NULL,
-  `Status` varchar(255) NOT NULL,
-  PRIMARY KEY (`ReserveID`)
-) ENGINE=MyISAM AUTO_INCREMENT=181 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
+  PRIMARY KEY (`adminid`),
+  KEY `empid` (`empid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 
 --
--- Table structure for table `returntable`
+-- Dumping data for table `tbadminacc`
 --
 
-DROP TABLE IF EXISTS `returntable`;
-CREATE TABLE IF NOT EXISTS `returntable` (
-  `ReturnID` int NOT NULL AUTO_INCREMENT,
-  `ReserveID` int NOT NULL,
-  `empid` int DEFAULT NULL,
-  `DateApproved` date DEFAULT NULL,
-  `DateReturn` date DEFAULT NULL,
-  `Status` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`ReturnID`)
-) ENGINE=MyISAM AUTO_INCREMENT=53 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `student table`
---
-
-DROP TABLE IF EXISTS `student table`;
-CREATE TABLE IF NOT EXISTS `student table` (
-  `userID` int NOT NULL AUTO_INCREMENT,
-  `studid` int NOT NULL,
-  `SR-Code` varchar(255) NOT NULL,
-  `Section` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `Password` varchar(255) NOT NULL,
-  PRIMARY KEY (`userID`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO `tbadminacc` (`adminid`, `empid`, `username`, `password`) VALUES
+(1, 1, 'nina', 'aguila');
 
 -- --------------------------------------------------------
 
@@ -115,7 +57,7 @@ CREATE TABLE IF NOT EXISTS `tbempinfo` (
   `firstname` varchar(25) NOT NULL,
   `department` varchar(20) NOT NULL,
   PRIMARY KEY (`empid`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `tbempinfo`
@@ -127,25 +69,129 @@ INSERT INTO `tbempinfo` (`empid`, `lastname`, `firstname`, `department`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbproductinfo`
+--
+
+DROP TABLE IF EXISTS `tbproductinfo`;
+CREATE TABLE IF NOT EXISTS `tbproductinfo` (
+  `itemid` int NOT NULL AUTO_INCREMENT,
+  `itemname` varchar(100) NOT NULL,
+  `item_img` mediumblob NOT NULL,
+  `sizes` varchar(50) NOT NULL,
+  `price` decimal(10,0) NOT NULL,
+  `stocks` int NOT NULL,
+  PRIMARY KEY (`itemid`),
+  UNIQUE KEY `itemid` (`itemid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbreservedetails`
+--
+
+DROP TABLE IF EXISTS `tbreservedetails`;
+CREATE TABLE IF NOT EXISTS `tbreservedetails` (
+  `reservationid` int NOT NULL AUTO_INCREMENT,
+  `itemid` int DEFAULT NULL,
+  `itemSize` varchar(50) NOT NULL,
+  `quantity` int NOT NULL,
+  `total_price` decimal(10,0) NOT NULL,
+  `SRcode` varchar(10) NOT NULL,
+  `reservation_date` date NOT NULL,
+  `status` varchar(255) DEFAULT 'pending',
+  PRIMARY KEY (`reservationid`),
+  UNIQUE KEY `reservationid` (`reservationid`),
+  UNIQUE KEY `reservationid_2` (`reservationid`),
+  KEY `itemid` (`itemid`),
+  KEY `studid` (`SRcode`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+
+--
+-- Dumping data for table `tbreservedetails`
+--
+
+INSERT INTO `tbreservedetails` (`reservationid`, `itemid`, `itemSize`, `quantity`, `total_price`, `SRcode`, `reservation_date`, `status`) VALUES
+(1, 9, 'Medium', 2, '700', '21-33878', '2023-11-23', 'accepted'),
+(2, 9, 'Medium', 7, '2450', '21-30452', '2023-11-23', 'accepted'),
+(3, 6, 'xl', 1, '350', '21-34134', '2023-11-24', 'pending');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbstatus`
+--
+
+DROP TABLE IF EXISTS `tbstatus`;
+CREATE TABLE IF NOT EXISTS `tbstatus` (
+  `reservationid` int DEFAULT NULL,
+  `resStatus` varchar(50) NOT NULL,
+  `statusNote` varchar(100) NOT NULL,
+  KEY `reservationid` (`reservationid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbstudacc`
+--
+
+DROP TABLE IF EXISTS `tbstudacc`;
+CREATE TABLE IF NOT EXISTS `tbstudacc` (
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `studid` int DEFAULT NULL,
+  `srcode` varchar(15) DEFAULT NULL,
+  UNIQUE KEY `srcode` (`srcode`),
+  KEY `studid` (`studid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Dumping data for table `tbstudacc`
+--
+
+INSERT INTO `tbstudacc` (`username`, `password`, `studid`, `srcode`) VALUES
+('Jerome', 'student123', 1, '21-30452'),
+('Max', 'student101', 2, '21-35348'),
+('Neil', 'nt3101', 3, '21-37635'),
+('Dom', 'kumarecakes01', 4, '21-33878'),
+('laud zion', 'Sayon19', 5, '21-34134');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbstudinfo`
 --
 
 DROP TABLE IF EXISTS `tbstudinfo`;
 CREATE TABLE IF NOT EXISTS `tbstudinfo` (
-  `studid` int NOT NULL AUTO_INCREMENT,
   `lastname` varchar(25) NOT NULL,
   `firstname` varchar(25) NOT NULL,
   `course` varchar(20) NOT NULL,
-  PRIMARY KEY (`studid`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `studid` int NOT NULL AUTO_INCREMENT,
+  UNIQUE KEY `studid` (`studid`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `tbstudinfo`
 --
 
-INSERT INTO `tbstudinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
-(1, 'parker', 'peter', 'bsit'),
-(2, 'kent', 'clark', 'bscs');
+INSERT INTO `tbstudinfo` (`lastname`, `firstname`, `course`, `studid`) VALUES
+('Panganiban', 'Jerome', 'BSIT-NT', 1),
+('Cabangisan', 'Dominic', 'BSIT', 2),
+('De Silva', 'Max Daniel', 'BSIT-NT', 3),
+('Pamintuan', 'Neil Daniel', 'BSIT-NT', 4),
+('cascalla', 'laud zion', 'BSIT NT - 3101', 5);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbstudacc`
+--
+ALTER TABLE `tbstudacc`
+  ADD CONSTRAINT `tbstudacc_ibfk_1` FOREIGN KEY (`studid`) REFERENCES `tbstudinfo` (`studid`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
